040299c6a3c0d8f3f5fa320f9a1275d835221896
fsdexter <fsdexter@gmail.com>
2022-01-24 10:42:02 +0100

    first database migrate

--- web_site/models.py
+++ web_site/models.py
@@ -1,21 +1,20 @@
-from curses import ACS_GEQUAL
-from turtle import setx
-from unicodedata import name
+
 from django.db import models
 
-# Create your models here.
+#Create your models here.
 
 class Owner(models.Model):
+    id = models.IntegerField(primary_key=True)
+    slug = models.SlugField(max_length=250)
     name = models.CharField(max_length=150)
-    age = models.IntegerField(max_length=3)
+    age = models.IntegerField()
     dni =models.CharField(max_length=15)
     phone = models.CharField(max_length=20)
-    slug = models.SlugField(max_length=250)
     
 class Pets(models.Model):
-    id = models.IntegerField(max_length=150)
-    age = models.IntegerField(max_length=3)
+    id = models.IntegerField(primary_key=True)
+    slug = models.SlugField(max_length=250)
+    age = models.IntegerField()
     name = models.CharField(max_length=150)
     species = models.CharField(max_length=150)
     sex = models.CharField(max_length=10)
-    slug = models.SlugField(max_length=250)
